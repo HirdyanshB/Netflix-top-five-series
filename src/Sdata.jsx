c6375253fd1b4fa040597f36bbc6dbad205fe36b
HirdyanshB <hirdyanshbhadauria2908@gmail.com>
2023-05-11 21:08:16 +0530

    initial

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {
+      imgsrc: "https://www.purplestonearts.in/wp-content/uploads/2021/12/Dark-Netflix-Origial-series-poster-art-1_11zon.webp",
+      title: "A Netflix Original Series",
+      sname: "DARK",
+      link: "https://www.netflix.com/in/title/80100172"
+    },
+
+    {
+      imgsrc: "https://images-cdn.ubuy.ae/6360cd939963074268417953-stranger-things-1-4-tv-show-poster.jpg",
+      title: "A Netflix Original Series",
+      sname: "Stranger Things",
+      link: "https://www.netflix.com/in/title/80057281"
+    },
+
+    {
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BMDEwOWVlY2EtMWI0ZC00OWVmLWJmZGItYTk3YjYzN2Y0YmFkXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+        title: "A Netflix Original Series",
+        sname: "Witcher",
+        link: "https://www.netflix.com/in/title/80189685"
+    },
+
+    {
+        imgsrc: "https://assets.gadgets360cdn.com/pricee/assets/product/202111/money_heist_season_5_1638276144.jpg",
+        title: "A Netflix Original Series",
+        sname:"Money Heist",
+        link: "https://www.netflix.com/in/title/80192098"
+    },
+
+    {
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BOTY0ZjliOGUtMDFiZS00YzcwLWFiZWYtMzg4MWZkMWIwYzYwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+        title: "A Netflix Original Series",
+        sname: "Shadow and Bone",
+        link: "https://www.netflix.com/in/title/80236319"
+    }
+];
+
+export default Sdata;
